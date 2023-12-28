145cf997eef30a32c2ee63bd31e63566f6ae1af3
Ayaz <ayaz702@mail.ru>
2023-12-28 20:35:59 +0300

    addZZ

---
+++ config/webpack.dev.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Development',
+        }),
+    ],
+    devServer: {
+        static: './dist',
+        hot: true,
+        port: 3001
+    },
+    stats: {
+        children: false,
+    },
+    devtool: 'inline-source-map'
+};
